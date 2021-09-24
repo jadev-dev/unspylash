d59b2ca81ce1637d42b1d6494fff0d156c56ee07
Jade Vogt <jade.vogt97@gmail.com>
2021-09-24 02:10:25 -0400

    fixed typing issues

--- unsplash.py
+++ unsplash.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ Allow easier access to the unsplash API """
-from typing import List, Literal
+from typing import Dict, List, Literal, Union
 import requests
+
 
 class UnsplashImage:
     """ Represent an image available via Unsplash """
@@ -19,6 +20,7 @@ class UnsplashImage:
         'webm',
         'webp',
         'blurhash',
+        None,
     ]
 
     crop_type = Literal[
@@ -29,21 +31,29 @@ class UnsplashImage:
         'faces',
         'entropy',
         'edges',
+        None,
     ]
 
-    def __init__(self, raw_url: str):
+    edits_type = Dict[str, Union[None, int, str]]
+
+    orientation_type = Literal['landscape', 'portrait', 'squarish', None]
+
+    def __init__(self, raw_url: str, user: str = None):
         self.raw_url = raw_url
-        self.edits = {
+        self.edits: UnsplashImage.edits_type = {
             'width': None,
             'height': None,
             'format': None,
             'fit': None,
         }
         self.crop = None
+        self.user = user
 
-    def adjust(self, width: int = None, height: int = None,
-               format_: img_format = None, crop: List[crop_type] = None,
-               fit: str = None):
+    def adjust(self, width: Union[int, None] = None,
+               height: Union[int, None] = None,
+               format_: img_format = None,
+               crop: List[crop_type] = None,
+               fit: Union[str, None] = None):
         """ Specify adjustments from default for the image """
         if width:
             self.edits['width'] = width
@@ -99,10 +109,16 @@ class Unsplash:
             return response.json()
         return {}
 
-    def get_random_image(self, query: str = None) -> UnsplashImage:
+    def get_random_image(
+            self, query: str = None,
+            orientation: UnsplashImage.orientation_type = None
+    ) -> UnsplashImage:
         """ Get a random image via the unsplash API """
         params = {}
         if query:
             params.update({'query': query})
+        if orientation:
+            params.update({'orientation': orientation})
         response = self.get('photos/random', params=params)
-        return UnsplashImage(response['urls']['raw'])
+        return UnsplashImage(response['urls']['raw'],
+                             user=response['user']['name'])
